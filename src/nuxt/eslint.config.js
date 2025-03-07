876496089351039c27071c1d4b6a1a4498fbc7e1
ryangst <ryangst.hire@gmail.com>
2025-03-07 16:54:48 -0300

    chore: remove Nuxt-specific configurations and dependencies

--- src/nuxt/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import nuxt from "../lint/nuxt.js";
-
-/**
- * @param {import("eslint").Linter.Config[]} config
- * @returns {import("eslint").Linter.Config[]}
- */
-export default function defineESLintConfig(config = []) {
-  return [...nuxt, ...config];
-}
